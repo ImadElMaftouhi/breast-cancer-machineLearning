d1d06f038ad5d5ae256be960ea8f922d529642e8
ImadElMaftouhi <imad.elmaft.wrk@gmail.com>
2024-03-21 14:42:56 +0000

    initial commit

---
+++ tuto_numpy.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 24 14:27:27 2024
+
+@author: imad
+"""
+
+
+import numpy as np
+
+arr = np.array([1,2,3,4])
+
+#1. Write a Numpy program to get the Numpy version and show the Numpy build configuration. 
+'''
+print(np.__version__)
+
+print(np.show_config())
+
+'''
+
+
+# 2. Write a NumPy program to get get help with the add function. 
+
+# print(np.info(np.add))
+
+# Write a NumPy program to test whether none of the elements of a given array are zero.  
+
+# print(arr.all())
+
+# 4. Write a NumPy program to test if any of the elements of a given array are non-zero.  
+
+# print(arr.any())
+
+# 5. Write a NumPy program to test a given array element-wise for finiteness (not infinity or not a number).         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
